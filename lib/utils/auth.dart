c1f157d50af3e3ce5f515c1671c7188166364cad
Unknown <coleweinman@gmail.com>
2018-07-05 21:42:56 -0500

    Added Firebase Cloud Messaging Support

--- lib/utils/auth.dart
+++ lib/utils/auth.dart
@@ -1,11 +1,22 @@
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'dart:async';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
+final FirebaseMessaging _firebaseMessaging = new FirebaseMessaging();
 FirebaseUser user;
 String userID;
 final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn _googleSignIn = new GoogleSignIn();
+
+Future initFCM() async{
+  _firebaseMessaging.requestNotificationPermissions();
+  _firebaseMessaging.configure(
+    onMessage: (Map<String,dynamic> message) {print('test');},
+    onLaunch: (Map<String,dynamic> message) {},
+    onResume: (Map<String,dynamic> message) {},
+  );
+}
 
 Future<bool> signInWithGoogle(bool force) async {
   GoogleSignInAccount googleUser;
